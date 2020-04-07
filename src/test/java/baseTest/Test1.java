60a8c819fea32b4e05ff34aec8001c486de2ac94
Sheela <pandikanda.sheel@gmail.com>
2020-04-07 13:22:01 +0530

    Second commit

--- src/test/java/baseTest/Test1.java
+++ src/test/java/baseTest/Test1.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 
 import java.util.concurrent.TimeUnit;
@@ -15,27 +19,27 @@ import java.util.concurrent.TimeUnit;
 public class Test1 {
 
 
-    public static String br = "chrome";
+    //    public static String br = "chrome";
     public static WebDriver driver;
 
-    public static void main(String[] args) throws InterruptedException {
-
-
+    @Parameters("browser")
+    @Test
+    public void setup(@Optional("chrome")String br) throws InterruptedException {
         if (br.equals("chrome")) {
 
             WebDriverManager.chromedriver().setup();
-             driver = new ChromeDriver();
-            driver.get("https://www.ig.com");
+            driver = new ChromeDriver();
+            driver.get("https://www.gmail.com");
         } else {
 
             WebDriverManager.iedriver().setup();
-             driver = new InternetExplorerDriver();
-            driver.get("https://www.ig.com");
+            driver = new InternetExplorerDriver();
+            driver.get("https://www.gmail.com");
 
         }
 //        Thread.sleep(3000);
         driver.manage().window().maximize();
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //        WebDriverWait wait = new WebDriverWait(driver,5);
 
         try {
@@ -45,11 +49,15 @@ public class Test1 {
             System.out.println("couldn't find the element");
         }
         driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("aaa");
-        driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/span/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[2]/div[2]/span")).getText();
+//        driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("aaa");
+//        driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/span/span")).click();
+//        driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[2]/div[2]/span")).getText();
 
 
+    }
+    @AfterTest
+    public void teardown() {
+        driver.quit();
     }
 
 }
